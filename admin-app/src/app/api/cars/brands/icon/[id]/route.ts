76689025277421f9c137526f211e31d71c7c7f0f
carservicewalekolkata <carservicewalekolkata@gmail.com>
2025-10-10 23:43:39 +0530

    changes

--- admin-app/src/app/api/cars/brands/icon/[id]/route.ts
+++ admin-app/src/app/api/cars/brands/icon/[id]/route.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextResponse } from "next/server";
 import { connectToDatabase } from "@/lib/db";
 import { Types } from "mongoose";
--- admin-app/src/app/api/cars/brands/route.ts
+++ admin-app/src/app/api/cars/brands/route.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { revalidateTag, unstable_cache } from "next/cache";
 import { NextResponse } from "next/server";
 
